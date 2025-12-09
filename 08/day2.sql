8bc2bba97b318516ad15a4c75901a83e4018f4af
Lukas Welsch <lukas.welsch@trustedshops.com>
2025-12-09 11:03:41 +0100

    feat: implemenation of day8

---
+++ 08/day2.sql
@@ -0,0 +1,64 @@
+-- INSTALL spatial;
+-- LOAD spatial;
+WITH points AS (
+    SELECT column0 as x, column1 as y, column2 as z, ROW_NUMBER() OVER() as rowid
+    FROM read_csv_auto('08/input.csv', header = false, delim=',')
+),
+all_edges AS (
+    SELECT
+        a.rowid AS src,
+        b.rowid AS dst,
+        (a.x - b.x)*(a.x - b.x)
+      + (a.y - b.y)*(a.y - b.y)
+      + (a.z - b.z)*(a.z - b.z) AS dist2, a.x, a.y, a.z, b.x, b.y, b.z
+    FROM points a
+    JOIN points b ON a.rowid < b.rowid
+),
+directed_edges AS (
+    SELECT dst, src, x, y, z, x_1, y_2, z_3
+    FROM all_edges
+    ORDER BY dist2
+-- I used a manual binary search to find the LIMIT that makes this components connected
+    LIMIT 5728
+),
+edges AS (
+    SELECT dst, src FROM directed_edges
+    UNION ALL
+    SELECT src, dst FROM directed_edges
+),
+nodes AS (
+    SELECT DISTINCT node
+    FROM (
+        SELECT src as node
+        FROM all_edges
+        UNION ALL
+        SELECT dst as node
+        FROM all_edges
+    )
+),
+weakly_components AS (
+    WITH RECURSIVE walks(node, front) AS (
+        SELECT node, node AS front
+        FROM nodes
+        UNION
+        SELECT walks.node, edges.dst AS front
+        FROM walks, edges
+        WHERE walks.front = edges.src
+    ),
+
+    components AS (
+        SELECT node, MIN(front) AS component
+        FROM walks
+        GROUP BY node
+    )
+
+    SELECT *
+    FROM components
+    ORDER BY component, node
+)
+-- SELECT component, COUNT(component)
+-- FROM weakly_components
+-- GROUP BY component
+-- ORDER BY COUNT(component) DESC
+
+SELECT * FROM directed_edges
