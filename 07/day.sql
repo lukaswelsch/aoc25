59f6c41cee75b527905877d95cdd3dcf7dd57f40
Lukas Welsch <lkswlsch@gmail.com>
2025-12-18 23:51:02 +0100

    fix: day7 done

--- 07/day.sql
+++ 07/day.sql
@@ -2,7 +2,7 @@ WITH data AS (
     SELECT
         string_split_regex(column0, '') AS arr,
         row_number() OVER () AS y
-    FROM read_csv_auto('07/example.csv', header = false, delim = '')
+    FROM read_csv_auto('07/input.csv', header = false, delim = '')
 ),
 grid AS (
 	SELECT UNNEST(arr) AS val, generate_subscripts(arr, 1) as x,
@@ -11,52 +11,57 @@ grid AS (
 	WHERE NOT arr <@ ['.']
 ),
 start AS (
-    SELECT x, y
+    SELECT x, y, val
     FROM grid
     WHERE val = 'S'
 ),
-recurse AS (
+t AS (
 	WITH RECURSIVE beams (
 		x,
-		y
+		y,
+		cnt
 	) AS (
-		--base case
-		SELECT
-			y + 1,
-			val
-		FROM start
+	    SELECT
+	        s.x,
+	        s.y + 1 AS y,
+	        1::BIGINT AS cnt
+	    FROM start s
 
-		UNION ALL
+	    UNION ALL
 
-		SELECT
-			b.y + 1 AS y,
-			b.val
+	    SELECT
+	        CASE
+	            WHEN g.val = '.' THEN b.x
+	            WHEN g.val = '^' AND lateral_step.side = 'L' THEN b.x - 1
+	            WHEN g.val = '^' AND lateral_step.side = 'R' THEN b.x + 1
+	        END AS nx,
+	        b.y + 1 AS ny,
+	        SUM(b.cnt) as cnt
 		FROM beams b
-		JOIN grid g ON g.y = b.y AND
+		JOIN grid g ON g.x = b.x AND g.y = b.y
+		CROSS JOIN (SELECT 'L' AS side UNION ALL SELECT 'R') AS lateral_step
+		WHERE (g.val = '.' AND lateral_step.side = 'L')
+		   OR (g.val = '^')
+		GROUP BY nx, ny
 	)
+	SELECT *
+	FROM beams
+),
+part1 AS (
+	SELECT COUNT(*) as result
+	 FROM t
+	JOIN grid USING(x,y)
+	WHERE grid.val = '^'
 
-
+),
+part2 AS (
+-- + 1 because we miss the count of the first S
+SELECT SUM(cnt) + 1 as result FROM t
+JOIN grid USING(x,y)
+WHERE grid.val = '^'
 )
-FROM grid
+SELECT result, 'part1' as part FROM part1
+UNION ALL
+SELECT result, 'part2' as part FROM part2
 
-
-
---,
---recurse AS (
---	WITH RECURSIVE tachyon (
---		x,
---        y,
---        cell,
---        RecursionStep
---    ) AS (
---        -- Base Case
---        SELECT
---            x,
---            y,
---
---
---    )
---
---
---)
 
